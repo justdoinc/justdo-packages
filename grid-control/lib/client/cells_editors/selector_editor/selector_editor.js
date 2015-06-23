7e6a005b198783adce44889682034b2069ffb1fb
Daniel C <333222@gmail.com>
2015-06-23 15:16:54 +0800

    grid-control: Selector editor: open the select on init, save and destory when picking an option

--- grid-control/lib/client/cells_editors/selector_editor/selector_editor.js
+++ grid-control/lib/client/cells_editors/selector_editor/selector_editor.js
@@ -2,6 +2,7 @@ PACK.Editors.SelectorEditor = function (args) {
     var $select;
     var currentValue;
     var scope = this;
+    var grid = args.grid;
 
     this.init = function () {
       var value, label, options, output = "";
@@ -21,8 +22,18 @@ PACK.Editors.SelectorEditor = function (args) {
       $select = $("<select>" + output + "</select>");
       $select.appendTo(args.container);
       $select.focus();
+      this.showOptions();
+      $select.click(function () {
+        grid.getEditorLock().commitCurrentEdit();
+      });
     };
 
+    this.showOptions = function () {
+      event = document.createEvent('MouseEvents');
+      event.initMouseEvent('mousedown', true, true, window);
+      $select.get(0).dispatchEvent(event);
+    };
+ 
     this.destroy = function () {
       $select.remove();
     };
