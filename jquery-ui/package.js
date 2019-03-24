11c8b2d0c236db9880be89232f55e5bfd27dbc5a
Daniel C <daniel@meteorspark.com>
2019-03-24 16:17:57 +0700

    jquery-ui: remove the use of build-fetcher, add jQuery-ui v1.12.1 directly to the tree

--- jquery-ui/package.js
+++ jquery-ui/package.js
@@ -10,9 +10,22 @@ Package.onUse(function (api) {
 
   api.use('jquery', 'client');
 
-  api.use("mizzao:build-fetcher@0.2.0", 'client');
+  api.addFiles('lib/jquery-ui.js', 'client');
 
-  api.add_files('jqueryui.fetch.json', 'client');
+  api.addFiles('lib/themes/smoothness/jquery-ui.min.css', 'client');
 
-  api.add_files('justdo-modifications/datepicker.js', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-icons_cd0a0a_256x240.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-icons_888888_256x240.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-bg_glass_75_dadada_1x400.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-icons_2e83ff_256x240.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-bg_glass_75_e6e6e6_1x400.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-bg_glass_65_ffffff_1x400.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-bg_glass_95_fef1ec_1x400.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-icons_222222_256x240.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-bg_highlight-soft_75_cccccc_1x100.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-bg_glass_55_fbf9ee_1x400.png', 'client');
+  api.addAssets('./lib/themes/smoothness/images/ui-icons_454545_256x240.png', 'client');
+
+  api.addFiles('justdo-modifications/datepicker.js', 'client');
+
 });
