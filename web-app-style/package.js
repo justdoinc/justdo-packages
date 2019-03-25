dbde474ae83d3dad263779fc27ba2c00875d4fe2
Daniel C <daniel@meteorspark.com>
2019-03-25 16:03:31 +0700

    Remove redundant bootstrap files

--- web-app-style/package.js
+++ web-app-style/package.js
@@ -5,18 +5,11 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-   api.versionsFrom("1.2.1");
+  api.versionsFrom("1.2.1");
 
-   api.use("twbs:bootstrap@3.3.5");
+  api.use("twbs:bootstrap@3.3.5");
 
-//   api.use("less");
-
-   api.use('fourseven:scss@3.2.0', client);
-
-//   api.addFiles('lib/custom.bootstrap.json', 'client');
-//   api.addFiles('lib/custom.bootstrap.mixins.import.less', 'client'); // This file doesn't really add anything but we add it for file changes tracking so it'll trigger rebuild
-//   api.addFiles('lib/custom.bootstrap.import.less', 'client'); // This file doesn't really add anything but we add it for file changes tracking so it'll trigger rebuild
-//   api.addFiles('lib/custom.bootstrap.less', 'client');
+  api.use('fourseven:scss@3.2.0', client);
 
   api.addFiles('lib/overrides.sass', 'client');
 });
