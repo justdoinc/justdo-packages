c3875f3bffa5777d300fb2a1418d6e9c632b8d9c
Kim Igori <kimigori@naver.com>
2019-03-27 15:07:39 +0700

    Introduce justdo-bootstrap-web-app-themes

---
+++ justdo-bootstrap-web-app-themes/package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: "justdoinc:bootstrap-themes",
+  summary: "",
+  version: "1.0.0"
+});
+
+client = "client"
+server = "server"
+both = [client, server]
+
+Package.onUse(function (api) {
+  api.versionsFrom("1.4.1.1");
+
+  // Switch themes with: APP.bootstrap_themes_manager.setTheme("sketchy")
+
+  api.addAssets("default/bootstrap.css", client);
+  api.addAssets("cerulean/bootstrap.css", client);
+  api.addAssets("minty/bootstrap.css", client);
+  api.addAssets("sandstone/bootstrap.css", client);
+  api.addAssets("sketchy/bootstrap.css", client);
+  api.addAssets("superhero/bootstrap.css", client);
+});
