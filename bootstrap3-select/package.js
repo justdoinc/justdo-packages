03c9ffcedc19b8c565d1660e66a444faa053e687
Daniel C <daniel@meteorspark.com>
2019-05-13 16:19:19 +0800

    bootstrap3-select: package.js updates for builder app generator

--- bootstrap3-select/package.js
+++ bootstrap3-select/package.js
@@ -1,15 +1,14 @@
+var path = Npm.require("path");
+var assets = path.join("bootstrap-select", "dist");
+
 Package.describe({
-    name: 'stemcapital:bootstrap3-select',
-    summary: 'Bootstrap 3 "select" styling, based on https://github.com/silviomoreto/bootstrap-select, forked from: https://github.com/leebenson/bootstrap3-select/',
-    version: '1.1.0'
+    name: "stemcapital:bootstrap3-select",
+    summary: "Bootstrap 3 'select' styling, based on https://github.com/silviomoreto/bootstrap-select, forked from: https://github.com/leebenson/bootstrap3-select/",
+    version: "1.1.0"
 });
 
 Package.onUse(function(api) {
-    var path = Npm.require('path');
-    var assets = path.join('bootstrap-select', 'dist');
-
-    api.addFiles([
-        path.join(assets, 'css', 'bootstrap-select.css'),
-        path.join('bootstrap-select/js/bootstrap-select.js')
-    ], 'client');
+    api.addFiles("bootstrap-select/css/bootstrap-select.css", "client");
+    api.addFiles("bootstrap-select/js/bootstrap-select.js", "client");
 });
+
