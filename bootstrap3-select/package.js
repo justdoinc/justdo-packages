123051ef356a5f8c6263b5de46193aeb70027dde
Daniel C <daniel@meteorspark.com>
2019-05-31 14:32:37 +0300

    bootstrap-select: get rid of redundant files, fix reference issue that caused the selectors to stop working properly in the web-app

--- bootstrap3-select/package.js
+++ bootstrap3-select/package.js
@@ -1,14 +1,10 @@
-var path = Npm.require("path");
-var assets = path.join("bootstrap-select", "dist");
-
 Package.describe({
     name: "stemcapital:bootstrap3-select",
-    summary: "Bootstrap 3 'select' styling, based on https://github.com/silviomoreto/bootstrap-select, forked from: https://github.com/leebenson/bootstrap3-select/",
     version: "1.1.0"
 });
 
 Package.onUse(function(api) {
-    api.addFiles("bootstrap-select/dist/css/bootstrap-select.css", "client");
-    api.addFiles("bootstrap-select/dist/js/bootstrap-select.js", "client");
+    api.addFiles("bootstrap-select.css", "client");
+    api.addFiles("bootstrap-select.js", "client");
 });
 
