9ec8a502bbbd62e1fad1c7c2e7b22f5ec42178ca
Daniel C <daniel@meteorspark.com>
2019-07-29 19:37:10 +0800

    justdo-linkify: add support for linkify-html

--- justdo-linkify/package.js
+++ justdo-linkify/package.js
@@ -1,12 +1,13 @@
 Package.describe({
-	name: 'justdoinc:justdo-linkify',
-	version: '0.0.1',
-	summary: ''
+  name: 'justdoinc:justdo-linkify',
+  version: '0.0.1',
+  summary: ''
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.1.0.3');
+  api.versionsFrom('1.1.0.3');
 
-	api.add_files('lib/linkify/linkify.min.js', 'client');
-	api.add_files('lib/linkify/linkify-string.min.js', 'client');
+  api.add_files('lib/linkify/linkify.min.js', 'client');
+  api.add_files('lib/linkify/linkify-string.min.js', 'client');
+  api.add_files('lib/linkify/linkify-html.min.js', 'client');
 });
