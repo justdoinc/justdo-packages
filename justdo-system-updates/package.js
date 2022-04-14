6463c62da6141580ef7c389effab66a12d2e6a2f
kimigori <kimigori@naver.com>
2022-04-14 14:46:37 +0900

    justdo-system-updates: v3_122_0

--- justdo-system-updates/package.js
+++ justdo-system-updates/package.js
@@ -159,24 +159,31 @@ Package.onUse(function (api) {
   // api.addAssets("lib/core-system-updates/v3-110/media/image_1.jpeg", client);
   // api.addAssets("lib/core-system-updates/v3-110/media/image_2.gif", client);
 
-  // v3_113
-  api.addFiles("lib/core-system-updates/v3-113/v3-113.sass", client);
-  api.addFiles("lib/core-system-updates/v3-113/v3-113.html", client);
-  api.addFiles("lib/core-system-updates/v3-113/v3-113.coffee", both);
-  api.addAssets("lib/core-system-updates/v3-113/media/image1.png", client);
+  // // v3_113
+  // api.addFiles("lib/core-system-updates/v3-113/v3-113.sass", client);
+  // api.addFiles("lib/core-system-updates/v3-113/v3-113.html", client);
+  // api.addFiles("lib/core-system-updates/v3-113/v3-113.coffee", both);
+  // api.addAssets("lib/core-system-updates/v3-113/media/image1.png", client);
 
   // v3_118_2
   api.addFiles("lib/core-system-updates/v3-118-2/v3-118-2.sass", client);
   api.addFiles("lib/core-system-updates/v3-118-2/v3-118-2.html", client);
   api.addFiles("lib/core-system-updates/v3-118-2/v3-118-2.coffee", both);
   api.addAssets("lib/core-system-updates/v3-118-2/media/image1.gif", client);
-
+  
   // v3_120_0
   api.addFiles("lib/core-system-updates/v3-120-0/v3-120-0.sass", client);
   api.addFiles("lib/core-system-updates/v3-120-0/v3-120-0.html", client);
   api.addFiles("lib/core-system-updates/v3-120-0/v3-120-0.coffee", both);
   api.addAssets("lib/core-system-updates/v3-120-0/media/image1.gif", client);
   api.addAssets("lib/core-system-updates/v3-120-0/media/image2.gif", client);
+
+  // v3_122_0
+  api.addFiles("lib/core-system-updates/v3-122-0/v3-122-0.sass", client);
+  api.addFiles("lib/core-system-updates/v3-122-0/v3-122-0.html", client);
+  api.addFiles("lib/core-system-updates/v3-122-0/v3-122-0.coffee", both);
+  api.addAssets("lib/core-system-updates/v3-122-0/media/image1.gif", client);
+  api.addAssets("lib/core-system-updates/v3-122-0/media/image2.gif", client);
 
   api.export("JustdoSystemUpdates", both);
 });
