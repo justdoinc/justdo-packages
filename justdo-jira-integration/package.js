e2b5d991245e94075cb31a75196a25a8b0a3ab2f
Brian Chan <brianyeung8@gmail.com>
2022-09-05 19:30:18 +0800

    justdo-jira-integration: Add grid-control-middlewares.coffee to package.js

--- justdo-jira-integration/package.js
+++ justdo-jira-integration/package.js
@@ -77,6 +77,7 @@ Package.onUse(function (api) {
   api.addFiles("lib/server/collections-indexes.coffee", server);
   api.addFiles("lib/server/methods.coffee", server);
   api.addFiles("lib/server/publications.coffee", server);
+  api.addFiles("lib/server/grid-control-middlewares.coffee", server);
 
   api.addFiles("lib/client/api.coffee", client);
   api.addFiles("lib/client/methods.coffee", client);
