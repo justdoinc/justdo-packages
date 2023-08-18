95bcf12b658d23eec7430f2fc9a21d54a38a94b8
Brian Chan <brianyeung8@gmail.com>
2023-08-18 17:25:55 +0800

    meteor-accounts-ui-bootstrap-3: Remove i18n related logic from accountsUIBootstrap3 obj #16390

--- meteor-accounts-ui-bootstrap-3/i18n.js
+++ meteor-accounts-ui-bootstrap-3/i18n.js
@@ -1,14 +1,2 @@
-i18n.setDefaultLanguage('en')
-
-accountsUIBootstrap3 = {
-  setLanguage: function (lang) {
-    return i18n.setLanguage(lang)
-  },
-  getLanguage: function () {
-    return i18n.getLanguage()
-  },
-  map: function (lang, obj) {
-    return i18n.map(lang, obj)
-  }
-}
+accountsUIBootstrap3 = {};
 
