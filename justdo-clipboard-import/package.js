57e47a8acd2735026b0a0b7b300d2af15ae5cd5c
Brian Chan <brianyeung8@gmail.com>
2023-10-18 15:40:50 +0800

    justdo-clipboard-import, justdo-i18n: Add zh-TW translation for clipboard import title

--- justdo-clipboard-import/package.js
+++ justdo-clipboard-import/package.js
@@ -52,6 +52,8 @@ Package.onUse(function (api) {
   api.use("iron:router@1.1.2", both);
 
   api.use("justdoinc:justdo-analytics@1.0.0", both);
+  api.use("tap:i18n@1.8.2", both);
+  api.use("justdoinc:justdo-i18n", client);
 
   api.use("matb33:collection-hooks@0.8.4", both);
 
--- justdo-i18n/package.js
+++ justdo-i18n/package.js
@@ -138,6 +138,8 @@ Package.onUse(function (api) {
   api.add_files("i18n/grid-control/en.i18n.json", both);
   api.add_files("i18n/justdo-orgs/zh-TW.i18n.json", both);
   api.add_files("i18n/justdo-orgs/en.i18n.json", both);
+  api.add_files("i18n/justdo-clipboard-import/zh-TW.i18n.json", both);
+  api.add_files("i18n/justdo-clipboard-import/en.i18n.json", both);
 
   // Uncomment only in packages that integrate with the main applications
   // Pure logic packages should avoid any app specific integration.
