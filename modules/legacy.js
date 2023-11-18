134a72f8b272e306689ddc20a914adec0a380740
Daniel C <daniel@meteorspark.com>
2023-11-18 18:44:51 +0700

    Fix a regression that caused the app and landing app not to start in legacy browsers (incl. Firefox on OSX, search engine crawlers, Pingdom, and maybe more) #16798

--- modules/legacy.js
+++ modules/legacy.js
@@ -1,4 +1,4 @@
 // The meteor-babel/plugins/sanitize-for-in-objects plugin generates code
 // that uses meteorBabelHelpers.sanitizeForInObject, but only when
 // compiling code for the web.browser.legacy bundle. See #10595.
-meteorBabelHelpers = require("meteor-babel-helpers");
+window.meteorBabelHelpers = require("meteor-babel-helpers");
