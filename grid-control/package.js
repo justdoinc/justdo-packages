a8b2adba7c34860c5dd336a2d2c894e374300ae1
Daniel C <daniel@meteorspark.com>
2023-11-27 13:58:06 +0700

    Remove version from all tap:i18n dependencies in package.js

--- grid-control/package.js
+++ grid-control/package.js
@@ -25,7 +25,7 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.1', both);
 
   api.use('copleykj:jquery-autosize@1.17.8', client);
-  api.use("tap:i18n@1.8.2", both);
+  api.use("tap:i18n", both);
   api.use('justdoinc:justdo-i18n@1.0.0', both);
 
   api.use('momentjs:moment@2.10.3', both);
