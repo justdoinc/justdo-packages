b70fc5626ab8176162c4760cbdfb51bbae039ce9
Daniel C <daniel@meteorspark.com>
2024-02-06 23:22:18 +0700

    Apply changes made by Meteor to package: 'accounts-base' between tags 2.6.1..2.7

--- accounts-base/package.js
+++ accounts-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'A user account system',
-  version: '2.2.1',
+  version: '2.2.2',
 });
 
 Package.onUse(api => {
@@ -61,6 +61,7 @@ Package.onTest(api => {
     'oauth-encryption',
     'ddp',
     'accounts-password',
+    'accounts-2fa',
   ]);
 
   api.addFiles('accounts_tests_setup.js', 'server');
