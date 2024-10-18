6a1f666e52e4dfafbdae786e37841c1dd9fd82bc
Brian Chan <brianyeung8@gmail.com>
2024-10-18 14:54:48 +0800

    meteor-bootboxjs: Remove bootbox close btn margin-top

--- meteor-bootboxjs/bootbox/bootbox.js
+++ meteor-bootboxjs/bootbox/bootbox.js
@@ -660,7 +660,7 @@
       if (options.title) {
         dialog.find(".modal-header").prepend(closeButton);
       } else {
-        closeButton.css("margin-top", "-10px").prependTo(body);
+        closeButton.prependTo(body);
       }
     }
 
