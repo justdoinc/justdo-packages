4336ec0c23f180b3b161caba6a40ff4dbd3167ea
Brian Chan <brianyeung8@gmail.com>
2024-11-22 12:03:32 +0800

    justdo-site-admins: Add random dependency

--- justdo-site-admins/package.js
+++ justdo-site-admins/package.js
@@ -46,6 +46,7 @@ Package.onUse(function (api) {
   api.use("meteorspark:logger@0.3.0", both);
   api.use("justdoinc:justdo-helpers@1.0.0", both);
   api.use("iron:router@1.1.2", both);
+  api.use("random", both);
 
   api.use("justdoinc:justdo-analytics@1.0.0", both);
 
